46663aed21c4f490976b0d6bab3a7a5e767b9b02
KTH Student <student@il2206.vm>
2018-09-18 17:02:09 +0000

    coucou

--- il2206-lab-master/hardware/DE2-pre-built/software/Lab1_test2/hello_world.c
+++ il2206-lab-master/hardware/DE2-pre-built/software/Lab1_test2/hello_world.c
@@ -15,10 +15,21 @@
  */
 
 #include <stdio.h>
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
 
 int main()
 {
-  printf("Hello from Nios II!\n");
-
+	int count = 0;
+	int delay;
+	printf("Hello from Nios II !!! !!!\n");
+	while(1){
+		IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_REDLED18_BASE, count & 0x01);
+		delay = 0;
+		while(delay < 50000){
+			delay++;
+		}
+		count++;
+	}
   return 0;
 }
