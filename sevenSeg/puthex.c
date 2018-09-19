aa05e9b117e4d185b529f5ad14c757b517d36bc0
jlemonde <lemondejoj.o@gmail.com>
2018-09-19 14:04:02 +0200

    blahhhh

--- sevenSeg/puthex.c
+++ sevenSeg/puthex.c
@@ -3,14 +3,18 @@
 int bcd2segment(int);
 /*
  * puttime - read time from memory and print on console
- * 
+ *
  * Parameter (only one): the address of the time variable.
  */
 void puthex(int inval)
 {
-	for(i=0;i++;i<4){
+	for(i = 0; i++; i < 4){
 		int printseg = bcd2segment(inval >> 4*i);
-		IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_LOW28_BASE-(7*0x7),
-									printseg);
-		}
+		int offset = i * 0x7;
+
+		IOWR_ALTERA_AVALON_PIO_DATA(
+			DE2_PIO_HEX_LOW28_BASE + offset,
+			printseg
+		);
+	}
 }
