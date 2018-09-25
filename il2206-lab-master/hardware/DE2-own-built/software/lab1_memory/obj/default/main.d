bc456af74ccd8d722b6e62e5681262ab1c42ad5d
KTH Student <student@il2206.vm>
2018-09-25 10:41:21 +0200

    a

---
+++ il2206-lab-master/hardware/DE2-own-built/software/lab1_memory/obj/default/main.d
@@ -0,0 +1,20 @@
+obj/default/main.o: main.c ../lab1_memory_bsp//HAL/inc/sys/alt_stdio.h \
+  ../lab1_memory_bsp/system.h ../lab1_memory_bsp/linker.h \
+  ../lab1_memory_bsp//drivers/inc/altera_avalon_performance_counter.h \
+  ../lab1_memory_bsp//HAL/inc/alt_types.h \
+  ../lab1_memory_bsp//HAL/inc/io.h \
+  ../lab1_memory_bsp//HAL/inc/alt_types.h
+
+../lab1_memory_bsp//HAL/inc/sys/alt_stdio.h:
+
+../lab1_memory_bsp/system.h:
+
+../lab1_memory_bsp/linker.h:
+
+../lab1_memory_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../lab1_memory_bsp//HAL/inc/alt_types.h:
+
+../lab1_memory_bsp//HAL/inc/io.h:
+
+../lab1_memory_bsp//HAL/inc/alt_types.h:
